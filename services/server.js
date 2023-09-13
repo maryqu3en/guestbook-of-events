4deba220509c46d4da3e2f10390666d0097701ab
kaouther <kaoutherboutheldja@gmail.com>
2023-09-13 21:44:52 +0100

    Update landing page

--- services/server.js
+++ services/server.js
@@ -5,12 +5,16 @@ const {authRouter} = require("./Router/Auth")
 const {postsRouter} = require("./Router/Post")
 const PORT = 8080
 app.set("view engine" , "pug")
-
+app.use(express.static('public'));
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.render('landingPage');
+  });
+  
 app.use("/Auth",authRouter)
 app.use("/posts" , postsRouter)
 app.listen(PORT , ()=>{
-    console.log("server is runing on ",PORT)
+    console.log("server is running on ",PORT)
 })
