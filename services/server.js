2b4a684f2cf6ee81e3aa037d57fa1f68fe576b6e
Meriem Soubih <soubih.meriem@gmail.com>
2023-09-14 14:31:53 +0100

    FEAT: login & register pages

--- services/server.js
+++ services/server.js
@@ -1,16 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
-const {authRouter} = require("./Router/Auth")
-const {postsRouter} = require("./Router/Post")
-const PORT = 8080
-app.set("view engine" , "pug")
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { authRouter } = require("./Router/Auth");
+const { postsRouter } = require("./Router/Post");
 
-app.use(cors())
-app.use(express.json())
+const PORT = 8080;
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/Auth",authRouter)
-app.use("/posts" , postsRouter)
-app.listen(PORT , ()=>{
-    console.log("server is runing on ",PORT)
-})
+app.use(cors());
+app.use(express.json());
+
+app.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  return res.render("register");
+});
+
+app.use("/Auth", authRouter);
+app.use("/posts", postsRouter);
+
+app.listen(PORT, () => {
+  console.log("server is runing on ", PORT);
+});
